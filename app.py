4836af81749453f491f9f27815e8038027d0ac33
maddy <maddy@makeskilled.com>
2023-05-12 15:50:24 +0530

    printed palindrome message

--- app.py
+++ app.py
@@ -5,5 +5,12 @@ print(type(a),type(b))
 
 # Check - Palindrome 
 
-print(a==a[::-1]) # 'radar' == 'radar' - True
-print(b==b[::-1]) # 'madhu' == 'uhdam' - False
+if a==a[::-1]:
+    print(a+ ' is a palindrome')
+else:
+    print(a+ ' is not a palindrome')
+
+if b==b[::-1]:
+    print(b + ' is a palindrome')
+else:
+    print(b + ' is not a palindrome')
