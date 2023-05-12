a2fa20c31f8ff59911f9075d3d0ee8a14a4e04f8
maddy <maddy@makeskilled.com>
2023-05-12 15:31:55 +0530

    modified hello world to palindrome string

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print('Hello Madhu')
+a='radar'
+b='madhu'
+
+print(type(a),type(b))
+
+# Check - Palindrome 
+
+print(a==a[::-1]) # 'radar' == 'radar' - True
+print(b==b[::-1]) # 'madhu' == 'uhdam' - False
