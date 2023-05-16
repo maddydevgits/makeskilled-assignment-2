019be33d4a9e11b43da209ed4f73a117cbda3e30
B. Ranadeer <davidbasiripogu@hotmail.com>
2023-05-16 15:32:24 +0530

    arthimetic operations

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 if __name__ == '__main__':
+    #Arithmetic Operations
     a = int(input())
     b = int(input())
     c = a + b
     d = a - b
     e = a * b
     print(c,d,e,sep = "\n")
+
+    print(f"{a // b}\n{a / b}")
