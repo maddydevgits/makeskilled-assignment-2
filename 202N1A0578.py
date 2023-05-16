c267ca54340a07108ca3a13f6ec40be3a21ffedc
shaik Musthafa <musthafaskkrnk@gmail.com>
2023-05-16 19:59:12 +0530

    updated the change

---
+++ 202N1A0578.py
@@ -0,0 +1,10 @@
+a='radar'
+b='madhu'
+
+print(type(a),type(b))
+
+# Check - Palindrome 
+
+print(a==a[::-1]) # 'radar' == 'radar' - True
+print(b==b[::-1]) # 'madhu' == 'uhdam' - False
+print("madhu")
