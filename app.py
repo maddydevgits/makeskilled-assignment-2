c7d906ce45665ad155c45e79420baef9b9e0796a
prakash524 <prakashprakash12944@gmail.com>
2023-05-17 11:57:55 -0400

    update the change

--- app.py
+++ app.py
@@ -1,24 +1,6 @@
-if __name__ == '__main__':
+a=4
+b=3
 
-#Arithmetic operators
-
- a = int(input())
-
- b = int(input())
-
-
- print("printing arithmetic operations on a & b:")
- print(a+b)# prints sum of two numbers a+b 
-
- print(a-b) #prints difference of two numbers a-b
-
- print(a*b) #prints product of two numbers10
-
-#Division 
-a = int(input())
-
-b= int(input())
-print("Prints division of a & b:")
-print(a//b) # prints floor result of division
-
-print(a/b) # prints approximation of division of two numbers
+print(a+b)
+print(a-b)
+print(a*b)
