cf3ea1f0f23e013b9a4531627677f50d91086824
Mounika <pinkymounika414@gmail.com>
2023-05-18 15:49:20 +0530

    arithemtic operations

---
+++ 202N1A0569m.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    #arithmetic operations
+    a = int(input())
+    b = int(input())
+    c = a + b
+    d = a - b
+    e = a * bprint(c,d,e,sep = '/n')
+    print(f"{a//b}\n{a/b}")
