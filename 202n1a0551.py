f4db37ec5d41ee21e178d8756a47a9e3602070a7
D.karthik <d8741598@gmail.com>
2023-06-09 20:49:51 +0530

    added a file

---
+++ 202n1a0551.py
@@ -0,0 +1,6 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+largest = max(num1, num2, num3)
+print("The largest number is:", largest)
