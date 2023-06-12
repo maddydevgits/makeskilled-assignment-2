5409379d866602f7a30cac64df2fa0f74896e25e
HIMANSHU <hansjangra0221@gmail.com>
2023-06-12 21:59:28 +0530

    New file added

---
+++ Assignment.py
@@ -0,0 +1,9 @@
+def factorial(x):
+ if x==0 or x==1:
+    return 1
+ else:
+   fact=x * factorial(x-1)
+   return fact
+ 
+ n = int(input("enter a number:"))
+ print(f"factorial of number {n}! is {factorial(n)}")
